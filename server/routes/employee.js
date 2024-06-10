727c7b9a8512d9d7694ec5eeb7c98e368cb5f59e
DeVonte <devonte.ellis@gmail.com>
2024-06-09 20:20:33 -0500

    json data structure added

---
+++ server/routes/employee.js
@@ -0,0 +1,38 @@
+[
+  {
+    "_id": "665e593b7880f0127fbdb220",
+    "empId": 1007,
+    "firstName": "John",
+    "lastName": "Brown"
+  },
+  {
+    "_id": "665e59627880f0127fbdb221",
+    "empId": 1008,
+    "firstName": "Sarah",
+    "lastName": "Johnson"
+  },
+  {
+    "_id": "665e59877880f0127fbdb222",
+    "empId": 1009,
+    "firstName": "Deion",
+    "lastName": "Smart"
+  },
+  {
+    "_id": "665e5b5a7880f0127fbdb224",
+    "empId": 1010,
+    "firstName": "Kelsey",
+    "lastName": "Warren"
+  },
+  {
+    "_id": "665e5e637880f0127fbdb225",
+    "empId": 1011,
+    "firstName": "Yolanda",
+    "lastName": "Jiminez"
+  },
+  {
+    "_id": "665e5e947880f0127fbdb226",
+    "empId": 1012,
+    "firstName": "Landon",
+    "lastName": "Collins"
+  },
+]
